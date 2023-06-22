c8884209187dd65e6001f6649841099df65e2a06
casunilsingla <137408465+casunilsingla@users.noreply.github.com>
2023-06-22 18:09:11 +0530

    back from app version scan.py

--- scan.py
+++ scan.py
@@ -1,15 +1,13 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
 import cv2
 from pyzbar import pyzbar
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Path to the Google Sheets credentials JSON file
-CREDENTIALS_FILE = 'scan-390611-25fc1daa0744.json'
+CREDENTIALS_FILE = 'path_to_credentials.json'
 
 # Google Sheets document ID
-DOCUMENT_ID = '1kTClIxeAcZhwXufiGUhxbJTn5X24fhIcjsxNS9Do9Ew'
+DOCUMENT_ID = 'your_document_id'
 
 # Connect to Google Sheets
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -54,70 +52,29 @@ def scan_barcodes():
     cv2.destroyAllWindows()
 
 # Manual barcode entry function
-def manual_entry(app, main_window):
-    barcode_input = QLineEdit()
-    submit_button = QPushButton("Submit")
-    quit_button = QPushButton("Quit")
-
-    def submit_action():
-        barcode_data = barcode_input.text()
+def manual_entry():
+    while True:
+        barcode_data = input("Enter Barcode (q to quit): ")
 
         if barcode_data == 'q':
-            app.quit()
+            break
 
         append_to_sheet(barcode_data)
-        barcode_input.clear()
-
-    submit_button.clicked.connect(submit_action)
-    quit_button.clicked.connect(app.quit)
-
-    widget = QWidget()
-    layout = QVBoxLayout()
-    layout.addWidget(QLabel("Enter Barcode (q to quit):"))
-    layout.addWidget(barcode_input)
-    layout.addWidget(submit_button)
-    layout.addWidget(quit_button)
-    widget.setLayout(layout)
-
-    # Set the manual entry widget as the central widget of the main_window
-    main_window.setCentralWidget(widget)
 
 # Main function
 def main():
-    app = QApplication(sys.argv)
-    main_window = QMainWindow()
-    main_window.setWindowTitle("Barcode Scanner & Data Entry App")
-    main_window.setGeometry(100, 100, 400, 300)
+    print("Barcode Scanner & Data Entry App")
+    print("1. Scan Barcodes")
+    print("2. Manual Entry")
 
-    scan_button = QPushButton("Scan Barcodes")
-    manual_button = QPushButton("Manual Entry")
-    quit_button = QPushButton("Quit")
+    choice = input("Select an option: ")
 
-    def scan_action():
+    if choice == '1':
         scan_barcodes()
-
-    def manual_action():
-        manual_entry(app, main_window)
-
-    def quit_action():
-        app.quit()
-
-    scan_button.clicked.connect(scan_action)
-    manual_button.clicked.connect(manual_action)
-    quit_button.clicked.connect(quit_action)
-
-    central_widget = QWidget(main_window)
-    layout = QVBoxLayout()
-    layout.addWidget(QLabel("Barcode Scanner & Data Entry App"))
-    layout.addWidget(scan_button)
-    layout.addWidget(manual_button)
-    layout.addWidget(quit_button)
-    central_widget.setLayout(layout)
-    main_window.setCentralWidget(central_widget)
-    main_window.show()
-
-    sys.exit(app.exec())
+    elif choice == '2':
+        manual_entry()
+    else:
+        print("Invalid choice. Exiting...")
 
 # Run the app
-if __name__ == '__main__':
-    main()
+main()
