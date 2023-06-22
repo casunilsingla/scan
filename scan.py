594e3d8932a09c4dbc8075a2a7acbc33b5f8be11
casunilsingla <137408465+casunilsingla@users.noreply.github.com>
2023-06-22 20:47:09 +0530

    sound added with invaildation scan.py

--- scan.py
+++ scan.py
@@ -2,6 +2,7 @@ import cv2
 from pyzbar import pyzbar
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import winsound
 
 # Path to the Google Sheets credentials JSON file
 CREDENTIALS_FILE = 'scan-390611-25fc1daa0744.json'
@@ -16,11 +17,47 @@ client = gspread.authorize(credentials)
 spreadsheet = client.open_by_key(DOCUMENT_ID)
 sheet = spreadsheet.sheet1
 
-# Function to append barcode data to Google Sheets
+import winsound
+
+# Function to append barcode data to Google Sheets and validate against sheet data
 def append_to_sheet(barcode_data):
-    row = [barcode_data]
-    sheet.append_row(row)
-    print("Data saved successfully.")
+    # Open Sheet 2 within the Google Sheet document
+    sheet_name = 'Sheet2'  # Replace with the name of Sheet 2
+    worksheet = spreadsheet.worksheet(sheet_name)
+
+    # Get all values from Sheet 2
+    sheet_values = worksheet.get_all_values()
+
+    # Search for the scanned barcode data in Sheet 2
+    for row in sheet_values:
+        if row[0] == barcode_data:
+            # Print the corresponding 3PL name and company name
+            if len(row) > 1:
+                print("3PL Name:", row[1])
+            if len(row) > 2:
+                company_name = row[2]
+                print("Company Name:", company_name)
+
+                # Play sound effect based on company name
+                if company_name == "Nayra":
+                    winsound.PlaySound("Nayra.wav", winsound.SND_FILENAME)  # Replace with the path to your sound effect file for Company A in WAV format
+                elif company_name == "Ni":
+                    winsound.PlaySound("Ni.wav", winsound.SND_FILENAME)  # Replace with the path to your sound effect file for Company B in WAV format
+                elif company_name == "DH":
+                    winsound.PlaySound("DH.wav", winsound.SND_FILENAME)  # Replace with the path to your sound effect file for Company B in WAV format
+                # Add more conditions for other company names and corresponding sound effects
+
+            # Append the barcode data to Sheet 1
+            sheet1 = spreadsheet.sheet1
+            row = [barcode_data]
+            sheet1.append_row(row)
+            print("Data saved successfully.")
+            break
+    else:
+        # Scanned barcode data not found in Sheet 2 or did not validate
+        print("Error: Barcode not found or data did not validate.")
+        # Play sound effect for invalid data
+        winsound.PlaySound("error.wav", winsound.SND_FILENAME)  # Replace with the path to your sound effect file for invalid data in WAV format
 
 # Barcode scanning function
 def scan_barcodes():
